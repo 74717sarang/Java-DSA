10e537d78e2224b368b3aac0ccf9f6437ce9b560
74717sarang <patilsarang13520@gmail.com>
2025-05-09 12:48:58 +0530

    new start test

--- Testing/src/com/newtest/Start.java
+++ Testing/src/com/newtest/Start.java
@@ -1,5 +1,6 @@
 package com.newtest;
 
+import java.util.Scanner;
 
 public class Start {
 
@@ -7,6 +8,21 @@ public class Start {
 		if (no == 0)
 			return 1;
 		return no * factoRecursion(no - 1);
+	}
+
+	private static boolean palidrom(String str) {
+
+		for (int i = 0; i < str.length() / 2; i++) {
+			if (str.charAt(i) != str.charAt(str.length() - 1 - i))
+				return false;
+		}
+
+		return true;
+	}
+
+	private static boolean findLeep(int year) {
+
+		return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
 	}
 
 	public static void main(String[] args) {
@@ -35,7 +51,7 @@ public class Start {
 		System.out.println("use while loop:" + fact);
 		no = 4;
 		fact = 1;
-		for (int i = 0; i<=no; i++) {
+		for (int i = 0; i <= no; i++) {
 			fact = no * fact;
 			no--;
 		}
@@ -43,50 +59,53 @@ public class Start {
 		System.out.println("use for loop:" + fact);
 
 		System.out.println();
-		
-		int n1=0;
-		int n2
-		=1;
+
+		int n1 = 0;
+		int n2 = 1;
 		int n3;
-		
+
 		System.out.println("fibonachi::");
-		System.out.print(n1+" ");
-		no=5;
-		for(int i=0;i<no;i++) {
-		n3=n1+n2;
-		System.out.print(n3+" ");
-		n1=n2;
-		n2=n3;
+		System.out.print(n1 + " ");
+		no = 5;
+		for (int i = 0; i < no; i++) {
+			n3 = n1 + n2;
+			System.out.print(n3 + " ");
+			n1 = n2;
+			n2 = n3;
 		}
 		System.out.println();
-		
-		int []arr= {1,5,3,9,8,2,7};
-		int max=0;
-		for(int i=0;i<arr.length;i++) {
-			max=max<arr[i]? arr[i]: max;
+
+		int[] arr = { 1, 5, 3, 9, 8, 2, 7 };
+		int max = 0;
+		for (int i = 0; i < arr.length; i++) {
+			max = max < arr[i] ? arr[i] : max;
 		}
-		System.out.println("max ele: "+max);
-		int secmax = 0,m3=0;
-		
-		for(int i=0;i<arr.length;i++) {
-			if (arr[i]>secmax && max > arr[i]) {
-			secmax=arr[i]; 	
+		System.out.println("max ele: " + max);
+		int secmax = 0, m3 = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > secmax && max > arr[i]) {
+				secmax = arr[i];
 			}
-		
+
 		}
-		System.out.println("secound high: "+secmax);
+		System.out.println("secound high: " + secmax);
+
+		int year = 2001;
+
+		System.out.println("leepyear:: " + findLeep(year));
+
+		String str = "abccba";
+		System.out.println("palidrom " + palidrom(str));
+
+		String op = new StringBuffer(str).reverse().toString();
+		System.out.println("direct palidrom " + op.equals(str));
 		
 		
-		
-		int year=2001;
-		
-		System.out.println("leepyear:: "+findLeep(year));
-		
+		Scanner sc=new Scanner(System.in);
+		int prime =sc.nextInt();
+		System.out.println("enter prime no: ");
 		
 	}
 
-	private static boolean findLeep(int year) {
-
-		return year%4==0&&year%100!=0||year%400==0;
-	}
 }
