e94040ad455507c4d17fec31b8ce767f8bd69c06
74717sarang <patilsarang13520@gmail.com>
2025-05-09 11:13:30 +0530

    new start test

--- Testing/src/com/newtest/Start.java
+++ Testing/src/com/newtest/Start.java
@@ -1,10 +1,13 @@
 package com.newtest;
 
+import java.util.Iterator;
+
 public class Start {
-	
-	public static  int factoRecursion(int no) {
-		if(no==0) return 1;
-		return no*factoRecursion(no-1); 
+
+	public static int factoRecursion(int no) {
+		if (no == 0)
+			return 1;
+		return no * factoRecursion(no - 1);
 	}
 
 	public static void main(String[] args) {
@@ -21,25 +24,38 @@ public class Start {
 		System.out.println("the no is :");
 
 		System.out.println(no % 2 == 0);
-		
-		no=4;
-		
-		System.out.println("factorial of No 4 :"+factoRecursion(no));
-		int fact=1;
-		while(no>0) {
-		 fact=no*fact;
-		no--;
-		}
-		System.out.println("use while loop:"+fact);
-		no=4;
-		fact=1;
-           for(int i=1;i<=no;i++) {
-        	   fact=no*fact;
-           }
-	
-		System.out.println("use for loop:"+fact);
 
-	
-	System.out.println("sdf");
+		no = 4;
+
+		System.out.println("factorial of No 4 :" + factoRecursion(no));
+		int fact = 1;
+		while (no > 0) {
+			fact = no * fact;
+			no--;
+		}
+		System.out.println("use while loop:" + fact);
+		no = 4;
+		fact = 1;
+		for (int i = 0; i<=no; i++) {
+			fact = no * fact;
+			no--;
+		}
+
+		System.out.println("use for loop:" + fact);
+
+		System.out.println();
+		
+		int n1=0;
+		int n2=1;
+		int n3;
+		
+		System.out.print(n1+" ");
+		no=5;
+		for(int i=0;i<no;i++) {
+		n3=n1+n2;
+		System.out.print(n3+" ");
+		n1=n2;
+		n2=n3;
+		}
 	}
 }
