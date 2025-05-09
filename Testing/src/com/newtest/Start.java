dc09bd628bd3695768297e7355f6445eccb8c6d4
74717sarang <patilsarang13520@gmail.com>
2025-05-09 09:38:28 +0530

    new start test

---
+++ Testing/src/com/newtest/Start.java
@@ -0,0 +1,20 @@
+package com.newtest;
+
+public class Start {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Wrods");
+//		System.out.println("aDD");
+
+		for (int i = 0; i < 10; i++) {
+			System.out.print("*" + " ");
+		}
+		System.out.println();
+
+		int no = 12;
+		System.out.println("the no is :");
+
+		System.out.println(no % 2 == 0);
+	}
+}
