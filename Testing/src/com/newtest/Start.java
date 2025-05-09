d8ae9b19a3e1089452fa4b12de64001f697006c7
74717sarang <patilsarang13520@gmail.com>
2025-05-09 10:59:58 +0530

    new start test

--- Testing/src/com/newtest/Start.java
+++ Testing/src/com/newtest/Start.java
@@ -1,6 +1,11 @@
 package com.newtest;
 
 public class Start {
+	
+	public static  int factoRecursion(int no) {
+		if(no==0) return 1;
+		return no*factoRecursion(no-1); 
+	}
 
 	public static void main(String[] args) {
 
@@ -16,5 +21,25 @@ public class Start {
 		System.out.println("the no is :");
 
 		System.out.println(no % 2 == 0);
+		
+		no=4;
+		
+		System.out.println("factorial of No 4 :"+factoRecursion(no));
+		int fact=1;
+		while(no>0) {
+		 fact=no*fact;
+		no--;
+		}
+		System.out.println("use while loop:"+fact);
+		no=4;
+		fact=1;
+           for(int i=1;i<=no;i++) {
+        	   fact=no*fact;
+           }
+	
+		System.out.println("use for loop:"+fact);
+
+	
+	System.out.println("sdf");
 	}
 }
