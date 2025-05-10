9e934b77f2ec49655b2a03f10e0040115d1bce6e
74717sarang <patilsarang13520@gmail.com>
2025-05-10 19:04:41 +0530

    new update day 6

--- Testing/src/com/newtest/test1.java
+++ Testing/src/com/newtest/test1.java
@@ -6,14 +6,14 @@ public class test1 {
 		String str = "abcaaawel wel wel z";
 		int[] arr = new int[27];
 
-		for (char ch : str.toCharArray()) {
-			if (ch != ' ') {
-				for(int j=ch+1;j<arr.length;j++) {
-					arr[i]++;
-				}
-//				int i = ch - '0' - 48;
-				System.out.println(i + " "+ch);
-			}
-		}
+//		for (char ch : str.toCharArray()) {
+//			if (ch != ' ') {
+//				for(int j=ch+1;j<arr.length;j++) {
+//					arr[i]++;
+//				}
+////				int i = ch - '0' - 48;
+//				System.out.println(i + " "+ch);
+//			}
+//		}
 	}
 }
