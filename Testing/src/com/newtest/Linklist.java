92dafd753aaf08bef42a93b913a3bc27bac23221
74717sarang <patilsarang13520@gmail.com>
2025-05-22 14:34:23 +0530

    test update

--- Testing/src/com/newtest/Linklist.java
+++ Testing/src/com/newtest/Linklist.java
@@ -2,4 +2,16 @@ package com.newtest;
 
 public class Linklist {
 
+	Node head;
+
+	public Linklist() {
+		head = null;
+	}
+
+	public Linklist(int data) {
+		if (head == null)
+			head = new Node(data);
+	}
+
+
 }
