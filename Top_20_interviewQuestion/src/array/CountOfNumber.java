d5eff35d1eb8b1c597c0403b6faf98d4c0aa8b5d
74717sarang <patilsarang13520@gmail.com>
2025-06-05 11:47:56 +0530

    top 20 Q update

--- Top_20_interviewQuestion/src/array/CountOfNumber.java
+++ Top_20_interviewQuestion/src/array/CountOfNumber.java
@@ -1,5 +1,10 @@
 package array;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class CountOfNumber {
 
 	public static void main(String[] args) {
@@ -7,27 +12,42 @@ public class CountOfNumber {
 		int[] arr = { 1, 2, 2, 3, 1, 4, 2, 4, 5 };
 
 		bruteForcr(arr);
+		advance(arr);
+
+	}
+
+	private static void advance(int[] arr) {
+
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i : arr) {
+			map.put(i, map.getOrDefault(i, 0) + 1);
+		}
+
+		for (Map.Entry<Integer, Integer> i : map.entrySet()) {
+			System.out.println(i.getKey() + " Count " + i.getValue());
+		}
 
 	}
 
 	private static void bruteForcr(int[] arr) {
-        
+
 		int count;
-		boolean[]visited=new boolean[arr.length];
-		for(int i=0;i<arr.length;i++) {
-			if(visited[i]) {
-				continue;   // it skip loop
+		boolean[] visited = new boolean[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			if (visited[i]) {
+				continue; // it skip loop
 			}
-			count=1;
-			for(int j=i+1;j<arr.length;j++) {
-				
-				if(arr[i]==arr[j]) {
+			count = 1;
+			for (int j = i + 1; j < arr.length; j++) {
+
+				if (arr[i] == arr[j]) {
 					count++;
-					visited[j]=true;
-					
+					visited[j] = true;
+
 				}
 			}
-			System.out.println(arr[i]+" Count "+count);
+			System.out.println(arr[i] + " Count " + count);
 		}
 	}
 
