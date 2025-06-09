44813e9af785ef07bffcf68bb8c3b564776dca6f
74717sarang <patilsarang13520@gmail.com>
2025-06-09 18:46:33 +0530

    top 20 Q update

--- Top_20_interviewQuestion/src/array/hamming_distance.java
+++ Top_20_interviewQuestion/src/array/hamming_distance.java
@@ -5,6 +5,56 @@ public class hamming_distance {
 //	Q9: Find a rotation with maximum hamming distance?
 	public static void main(String[] args) {
 
+		int[] arr = { 1, 4, 1 };
+		System.out.println("Max Hamming Distance: " + bruteForce(arr));
+		System.out.println("Max Hamming Distance: " + advance(arr));
+
+	}
+
+	private static int advance(int[] arr) {
+		 int n = arr.length;
+	        int[] brr = new int[2 * n];
+	        // Concatenate array to itself
+	        for (int i = 0; i < n; i++) {
+	            brr[i] = arr[i];
+	            brr[i + n] = arr[i];
+	        }
+
+	        int maxHamming = 0;
+
+	        // Only need to check first n-1 rotations
+	        for (int i = 1; i < n; i++) {
+	            int currHamming = 0;
+	            for (int j = 0; j < n; j++) {
+	                if (arr[j] != brr[i + j]) {
+	                    currHamming++;
+	                }
+	            }
+	            maxHamming = Math.max(maxHamming, currHamming);
+
+	            // If we get max possible Hamming distance (n), break
+	            if (maxHamming == n) {
+	                break;
+	            }
+	        }
+
+	        return maxHamming;
+	}
+
+	private static int bruteForce(int[] arr) {
+
+		int distance = 0;
+		for (int i = 1; i < arr.length; i++) {
+			int current = 0;
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[j] != arr[(i + j) % arr.length]) {
+					current++;
+				}
+			}
+			distance = Math.max(distance, current);
+		}
+
+		return distance;
 	}
 
 }
--- Top_20_interviewQuestion/src/string/Repeated_Subsequence.java
+++ Top_20_interviewQuestion/src/string/Repeated_Subsequence.java
@@ -2,4 +2,50 @@ package string;
 
 public class Repeated_Subsequence {
 
+//	Q20: Check if a repeated subsequence is present in a string or not
+
+	public static void main(String[] args) {
+		String str = "aabb";
+		System.out.println("Has repeated subsequence: " + bruteforce(str));
+		System.out.println("Has repeated subsequence: " + advance(str));
+
+	}
+
+	private static boolean advance(String s) {
+		 int n = s.length();
+	        int[][] dp = new int[n + 1][n + 1];
+
+	        // Build DP table similar to finding LCS, ensuring i != j
+	        for (int i = 1; i <= n; i++) {
+	            for (int j = 1; j <= n; j++) {
+	                if (s.charAt(i - 1) == s.charAt(j - 1) && i != j) {
+	                    dp[i][j] = 1 + dp[i - 1][j - 1];
+	                } else {
+	                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+	                }
+	            }
+	        }
+
+	        // If length of longest repeated subsequence >= 2
+	        return dp[n][n] >= 2;	
+}
+
+	private static boolean bruteforce(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = i + 1; j < str.length(); j++) {
+				if (str.charAt(i) == str.charAt(j)) {
+					// Check if there is another occurrence after j
+					for (int k = j + 1; k < str.length(); k++) {
+						if (str.charAt(k) == str.charAt(j)) {
+							return true;
+
+						}
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
 }
