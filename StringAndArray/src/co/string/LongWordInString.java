83e0aa93e672f241f5e8f6ec1c939d77f088b52d
74717sarang <patilsarang13520@gmail.com>
2025-06-21 10:59:32 +0530

    struc change

---
+++ StringAndArray/src/co/string/LongWordInString.java
@@ -0,0 +1,31 @@
+package co.string;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LongWordInString {
+
+	public static void main(String[] args) {
+		String sentence = "Flip the words in a sentence";
+
+		String[] arr = sentence.split(" ");
+		String result = "";
+		for (String s : arr) {
+			result = result.length() < s.length() ? s : result;
+		}
+		System.out.println(result);
+
+		List<String> list = new ArrayList<>(Arrays.asList(arr));
+
+	 	String re = list.stream().max(Comparator.comparingInt(String::length)).orElse("");
+	        
+	
+	 	String s = list.stream().sorted((a,b)->Integer.compare( b.length(),a.length())).findFirst().orElse("");
+	
+	System.out.println("direct  "+s);
+	
+	}
+
+}
