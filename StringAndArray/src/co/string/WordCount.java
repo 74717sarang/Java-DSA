b2b55d732721af4ac38f3dafc54533e9c2f90daa
74717sarang <patilsarang13520@gmail.com>
2025-06-23 10:57:42 +0530

    string and array update

---
+++ StringAndArray/src/co/string/WordCount.java
@@ -0,0 +1,11 @@
+package co.string;
+
+public class WordCount {
+	public static void main(String[] args) {
+
+		
+		String str="ad asd dfsd";
+		System.out.println( str.split(" ").length );
+				
+	}
+}
