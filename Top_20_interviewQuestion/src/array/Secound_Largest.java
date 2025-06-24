03ed59969f29467cf3ac51de7ff0eb40c5678916
74717sarang <patilsarang13520@gmail.com>
2025-06-24 18:23:41 +0530

    string and array update

--- Top_20_interviewQuestion/src/array/Secound_Largest.java
+++ Top_20_interviewQuestion/src/array/Secound_Largest.java
@@ -36,14 +36,57 @@ public class Secound_Largest {
 		return secondLarge;
 	}
 
-	public static void main(String[] args) {
+	
+	public static  int thirdLarge(int []arr) {
+		
+		int large=arr[0];             // set first is large
+		int secondlarge = arr[0],th=arr[0];         //set first is secondlarge 
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]>large) {
+				th=secondlarge;
+				secondlarge=large;
+				large=arr[i];
+				
+			}
+			else if(arr[i]<large && arr[i]>secondlarge) {
+				th=secondlarge;
+				secondlarge=arr[i];
+			}
+		}
+		return th;
+	}
+	
+	
+	static public  int secondMin(int arr[]) {
+		int min=arr[1];
+		int secondmin=arr[0];
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]<min) {
+				secondmin=min;
+				min=arr[i];
+			}
+			else if(arr[i]<secondmin  && arr[i]>min)	{
+				secondmin=arr[i];
+			}
+		}
+      return secondmin;
+	}
+	
+	static public  void main(String[] args) {
 
-		int[] arr = { 1, 5, 39, 6, 9, 5, 99 };
+		int[] arr = { 1, 5, 39, 6, 9, 35, 99 };
 
 		System.out.println("Brute_force");
 		System.out.println(brute_force(arr));
 		System.out.println("Advance");
 		System.out.println(advance(arr));
+		System.out.println("new fun thirdLarge");
+		System.out.println(thirdLarge(arr));
+
+		System.out.println("new fun secondMin");
+		System.out.println(secondMin(arr));
 	}
 
 }
