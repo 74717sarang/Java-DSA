637baad14c220922ac0d9949610af538b890f319
74717sarang <patilsarang13520@gmail.com>
2025-06-30 09:18:12 +0530

    string and array update

--- Testing/src/com/newtest/t.java
+++ Testing/src/com/newtest/t.java
@@ -2,8 +2,18 @@ package com.newtest;
 
 public class t {
 
+	String str="adc";
+	
+     	public  int number() {
+		return 0;
+	}
+	
+	
 	public static void main(String[] args) {
-		
+		final int no;
+		 int n=1;
+		 no=1;
+		System.out.println(no);
 		
 		
 	}
