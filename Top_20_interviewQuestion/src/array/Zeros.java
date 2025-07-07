038d5bc9c7603b34d74db8ac0015bdb1f8d09ace
74717sarang <patilsarang13520@gmail.com>
2025-07-07 14:37:57 +0530

    add new

---
+++ Top_20_interviewQuestion/src/array/Zeros.java
@@ -0,0 +1,49 @@
+package array;
+
+import java.util.Arrays;
+
+public class Zeros {
+
+	public static void main(String[] args) {
+		int[] arr = { 9, 8, 7, 0, 5, 0, 3, 0 };
+//		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		leftMoveZeros(arr);
+		System.out.println("New right Move");
+		rightMoveZeros(arr);
+	}
+
+	private static void rightMoveZeros(int[] arr) {
+		int len = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != 0) {
+				arr[len++] = arr[i];
+			}
+		}
+		while (len < arr.length) {
+			arr[len++] = 0;
+		}
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+	private static void leftMoveZeros(int[] arr) {
+
+		int len = arr.length;
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] != 0) {
+				arr[--len] = arr[i];
+			}
+		}
+
+		while (len >= 0) {
+			arr[len--] = 0;
+		}
+
+		System.out.println("New Left Move");
+
+		System.out.println(Arrays.toString(arr));
+
+	}
+}
