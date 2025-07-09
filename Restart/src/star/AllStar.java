5dae81ac84e5467fb4ca1a2b78b0be5e7c8c4c8b
74717sarang <patilsarang13520@gmail.com>
2025-07-09 14:31:57 +0530

    add new restart pkg

--- Restart/src/star/AllStar.java
+++ Restart/src/star/AllStar.java
@@ -9,10 +9,28 @@ public class AllStar {
 			for (int j = 0; j < n - i - 1; j++) {
 				System.out.print(" ");
 			}
-			for (int k = 0; k < 2*i+1; k++) {
+			for (int k = 0; k < 2 * i + 1; k++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+		
+		System.out.println("------    -------");
+		
+		
+		
+		for(int i=n;i>=0;i--) {
+			for(int j=0;j<n-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=0;k<2*i+1;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		System.out.println("------    -------");
+
 	}
 }
