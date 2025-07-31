1fc2ed511c0e701b0cafa9fd4c498e9ac0a91f64
74717sarang <patilsarang13520@gmail.com>
2025-07-31 11:08:01 +0530

    update

--- idigiClound/Test_idigiCloud/src/com/test/Question2.java
+++ idigiClound/Test_idigiCloud/src/com/test/Question2.java
@@ -4,21 +4,43 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Question2 {
-
+	// 9*9 matrix sudoko valid
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 for(;;) {
-	    	   System.out.println("Hii");
-	       }
+		char[][] board = { { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+				{ '6', '.', '.', '1', '9', '5', '.', '.', '.' }, { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+				{ '8', '.', '.', '.', '6', '.', '.', '.', '3' }, { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+				{ '7', '.', '.', '.', '2', '.', '.', '.', '6' }, { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+				{ '.', '.', '.', '4', '1', '9', '.', '.', '5' }, { '.', '.', '.', '.', '8', '.', '.', '7', '9' } };
+
+		boolean result = isValidSudoku(board);
+		System.out.println("Is the Sudoku board valid? " + result);
 	}
-	public boolean sudoko(int[][]arrr) {
-		
-     Set<Integer>colSet=new HashSet<>();
-     Set<Integer>rowSet=new  HashSet<>();
-     
-       for(;;) {
-    	   System.out.println("Hii");
-       }
+
+	public static boolean isValidSudoku(char[][] arr) {
+
+		boolean[][] row = new boolean[9][9];
+		boolean[][] col = new boolean[9][9];
+		boolean[][] box = new boolean[9][9];
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+
+				if (arr[i][j] != '.') {
+					int num = arr[i][j] - '1';
+					int boxIndex = (i / 3) * 3 + (j / 3);
+
+					if(row[i][num] || col[j][num] || box[boxIndex][num]) {
+						return false;
+					}
+					row[i][num]=true;
+				    col[j][num] =true;
+				    box[boxIndex][num]=true;
+				}
+
+			}
+		}
+
+		return true;
 	}
 
 }
