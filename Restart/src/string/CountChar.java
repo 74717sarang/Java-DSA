f4192fcf9992a3a97adc408c588e43548c31a98d
74717sarang <patilsarang13520@gmail.com>
2025-08-08 10:13:29 +0530

    update

--- Restart/src/string/CountChar.java
+++ Restart/src/string/CountChar.java
@@ -45,7 +45,7 @@ public class CountChar {
 				}
 			}
 			if (arr[i] != '\0')
-				  System.out.println(arr[i] + " Counts " + count[i]);
+				System.out.println(arr[i] + " Counts " + count[i]);
 		}
 
 		for (int i = 0; i < arr.length; i++)
